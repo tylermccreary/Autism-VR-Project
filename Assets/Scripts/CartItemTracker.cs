de26e2b36e8d1a3399e7db30caedef2ca9f83161
tylermccreary <tjmccreary@gmail.com>
2020-02-26 17:53:36 -0600

    Adding items to cart

---
+++ Assets/Scripts/CartItemTracker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartItemTracker : MonoBehaviour
+{
+    private List<Item> items;
+    public List<Item> ItemsList { get { return items; } }
+
+    public delegate void CartContentsChangeEvent();
+    public static event CartContentsChangeEvent OnCartContentsChange;
+
+    private void Awake() {
+        items = new List<Item>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Item item = other.gameObject.GetComponent<Item>();
+        if (item != null) {
+            items.Add(item);
+        }
+        Debug.Log(items.Count);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Item item = other.gameObject.GetComponent<Item>();
+        if (item != null) {
+            items.Remove(item);
+        }
+        Debug.Log(items.Count);
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemNameEnum {
+    Carrot
+}
+
+public enum ItemTypeEnum {
+    Fruit,
+    Vegetable
+}
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private ItemNameEnum name;
+    [SerializeField] private ItemTypeEnum itemType;
+    public ItemNameEnum Name { get { return name; } set { name = value; } }
+    public ItemTypeEnum ItemType { get { return itemType; } set { itemType = value; } }
+}
