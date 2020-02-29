dcfc9935cc72c70985af8fd220212ce4106770e7
tylermccreary <tjmccreary@gmail.com>
2020-02-28 18:50:56 -0600

    Grocery list

---
+++ Assets/Scripts/ShoppingList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingList : MonoBehaviour
+{
+    List<ShoppingListItem> list = new List<ShoppingListItem>();
+
+    private void Start() {
+        CreateRandomizedShoppingList();
+    }
+
+    private void CreateRandomizedShoppingList() {
+        //TODO
+    }
+}
---
+++ Assets/Scripts/ShoppingListItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShoppingListItem : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Item item;
+    [SerializeField] private int quantity;
+    private bool collected;
+
+
+}
