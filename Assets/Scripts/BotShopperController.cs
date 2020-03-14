6e6ad6404eb3637e85631f8f689a7631bdf30090
tylermccreary <tjmccreary@gmail.com>
2020-03-14 16:54:40 -0500

    Bots spawning

---
+++ Assets/Scripts/BotShopperController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotShopperController : MonoBehaviour
+{
+    [SerializeField] Transform cartSpawnPoints;
+    private List<BotShoppingListItem> botShoppingList;
+    public bool finishedShopping = false;
+    public bool startInside = false;
+
+    private int currentItemIndex = 0;
+    private bool searchingForItem = false;
+    private bool grabbingItem = false;
+    private bool leaving = false;
+
+    private void Start() {
+        botShoppingList = new List<BotShoppingListItem>();
+        int numberOfItems = Random.Range(2, 12);
+
+        GameObject[] pickUpAreaArray = GameObject.FindGameObjectsWithTag(Tag.PICK_UP_AREA);
+        for (int i = 0; i < numberOfItems; i++) {
+            int itemIndex = Random.Range(0, pickUpAreaArray.Length);
+            GameObject pickUpArea = pickUpAreaArray[itemIndex];
+
+            ItemGrabArea itemGrabArea = pickUpArea.GetComponent<ItemGrabArea>();
+            Item item = itemGrabArea.item;
+            BotShoppingListItem botShoppingListItem = new BotShoppingListItem(item, itemGrabArea.transform);
+            botShoppingList.Add(botShoppingListItem);
+        }
+    }
+
+    private void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/BotSpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BotSpawner : MonoBehaviour
+{
+    [SerializeField] private float spawnRate;
+    [SerializeField] private int maxSpawnedAtOnce;
+    [SerializeField] private Collider aCollider;
+    [SerializeField] Transform[] spawnLocationsOutside;
+    [SerializeField] Transform[] spawnLocationsInside;
+    [SerializeField] GameObject botPrefab;
+
+    private int nextPriority = 10;
+    private int totalShoppersShopping = 0;
+
+    private float timeSinceLastSpawn = 0;
+
+    private void Start() {
+        SpawnCurrentShoppers();
+    }
+
+    private void Update() {
+        /*timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn > spawnRate) {
+            timeSinceLastSpawn = 0f;
+            if (totalShoppersShopping < maxSpawnedAtOnce) {
+                int spawnIndex = Random.Range(0, spawnLocationsOutside.Length);
+                SpawnShopper(spawnLocationsOutside[spawnIndex]);
+            }
+        }*/
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        BotShopperController botShopperController = other.gameObject.GetComponent<BotShopperController>();
+        if (botShopperController != null && botShopperController.finishedShopping) {
+            Destroy(other.gameObject);
+            totalShoppersShopping -= 1;
+        }
+    }
+
+    private void SpawnCurrentShoppers() {
+        int amountToSpawn = Mathf.Min((int)(maxSpawnedAtOnce * 0.8f), spawnLocationsInside.Length);
+        for (int i = 0; i < amountToSpawn; i++) {
+            SpawnShopper(spawnLocationsInside[i]);
+        }
+    }
+
+    private void SpawnShopper(Transform locationTransform) {
+        GameObject spawnedBot = Instantiate(botPrefab, locationTransform);
+        NavMeshAgent agent = spawnedBot.GetComponent<NavMeshAgent>();
+        agent.avoidancePriority = nextPriority;
+        totalShoppersShopping += 1;
+        nextPriority += 1;
+    }
+}
